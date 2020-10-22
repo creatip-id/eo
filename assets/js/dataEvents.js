130d107a2dd774152e4d5c769b761494bf689863
waidzk <waidzk30@gmail.com>
2020-10-22 21:09:39 +0700

    Add events section
    22/10/20

---
+++ assets/js/dataEvents.js
@@ -0,0 +1,44 @@
+const events = [
+  {
+    title : 'Light up concert 2020',
+    desc : 'Lorem incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image : 'assets/img/evnt1.jpg',
+    sec : '500'
+  },
+  {
+    title : 'Color Rain Festival 2020',
+    desc : 'Lorem incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image : 'assets/img/evnt2.jpg',
+    sec : '1000'
+  },
+  {
+    title : ' Birthday of lifeleaf company',
+    desc : 'Lorem incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image : 'assets/img/evnt3.jpg',
+    sec : '1500'
+  },
+  {
+    title : 'The Wedding of Adam and Girl',
+    desc : 'Lorem incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image : 'assets/img/evnt4.jpg',
+    sec : '2000'
+  },
+  {
+    title : 'The Wedding of Adam and Girl2',
+    desc : 'Lorem incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image : 'assets/img/evnt5.jpg',
+    sec : '2500'
+  },
+  {
+    title : 'New Year Celebration In Wakanda City',
+    desc : 'Lorem incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image : 'assets/img/evnt6.jpg',
+    sec : '3000'
+  },
+  {
+    title : 'Traditional Balinese festival',
+    desc : 'Lorem incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image : 'assets/img/evnt7.jpg',
+    sec : '3500'
+  }
+]
---
+++ assets/js/ourJs.js
@@ -0,0 +1,41 @@
+// SLICK SLIDER
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+// DYNAMIC DATA
+const list = document.querySelector('#list');
+list.innerHTML = "";
+events.forEach(evnt => {
+  list.innerHTML += `
+    <div class="slide"><img src="${evnt.image}" onclick="detail('${evnt.title}','${evnt.desc}','${evnt.image}','${evnt.join}')"></div>
+  `
+})
+
+function detail(nama, desc, imgD, join) {
+  console.log(join);
+  let detail = document.querySelector("#detail");
+  detail.querySelector('.eventImg').src = imgD;
+  detail.querySelector('.eventTitle').innerHTML = nama;
+  detail.querySelector('.eventDesc').innerHTML = desc;
+}
